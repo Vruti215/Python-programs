f220171545802d681e3cd17d3b456d3930c7e905
Vruti215 <pujaravruti@gmail.com>
2026-03-13 11:48:41 +0530

    Add files via upload

---
+++ Unit-2/4.py
@@ -0,0 +1,13 @@
+from mymodule import *
+
+#import mymodule
+
+a=int(input("enter number 1: "))
+b=int(input("enter number 2: "))
+print("Following are operations performed on numbers:")
+
+print("Addition:",addition(a,b))
+print("Subtraction:",subtraction(a,b))
+print("Multiplication:",multiplication(a,b))
+print("Division:",division(a,b))
+
